adfd7578e4014c5ab55667c45d2857cff261b438
meelllkins <elkin.palomino@correo.tdea.edu.co>
2026-08-04 08:05:14 -0500

    Revert "feat: actualizar favicon y mejorar la configuración de Vite para incluir el nuevo icono"
    This reverts commit 2e6f8835008459c6bf62b229efff64fb772c3584.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'icon-192.png', 'icon-512.png'],
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'Frigorinus Logística',
         short_name: 'Frigorinus',
